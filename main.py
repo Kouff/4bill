065472fcede7940917d71f7fffdadf323efa04a2
Kouff <thematuhaimmortal@gmail.com>
2020-12-12 21:12:32 +0200

    add a exception handler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
+from starlette.exceptions import HTTPException
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 
@@ -12,8 +13,15 @@ class Request(HTTPEndpoint):
         return JSONResponse({'result': 'OK'})
 
 
+async def http_exception(request, exc):
+    return JSONResponse({"error": exc.detail}, status_code=exc.status_code)
+
+
+exception_handlers = {
+    HTTPException: http_exception
+}
 routes = [
     Route('/request/{amount:int}', Request),
 ]
 
-app = Starlette(debug=True, routes=routes)
+app = Starlette(debug=True, routes=routes, exception_handlers=exception_handlers)
