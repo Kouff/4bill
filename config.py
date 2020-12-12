28971178efa37c7785ca9e3161ffdef5fd3f9413
Kouff <thematuhaimmortal@gmail.com>
2020-12-12 23:04:35 +0200

    add comments and typing

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-AMOUNT_LIMITS_CONFIG = {10: 2, 30: 5, 60: 7}
+# AMOUNT_LIMITS_CONFIG can have an unlimited number of key:value.
+# AMOUNT_LIMITS_CONFIG must be a dict. The key is seconds and the value is limit. {seconds: limit}
+# Key and value must be an integer. {60: 3000}
+# Checking runs from left to right.
+AMOUNT_LIMITS_CONFIG: dict = {10: 1000, 60: 3000, 3600: 20000}
--- limiter.py
+++ limiter.py
@@ -11,25 +11,27 @@ class Limiter:
         self.func = func
 
     def __call__(self, *args, **kwargs):
-        now = int(time())
-        limit = self.is_limited(now)
-        if limit is None:
+        now: int = int(time())  # get the current time
+        limit: str or None = self.is_limited(now)  # get error message if the limit is exceeded else None
+        if limit is None:  # if the limit is not exceeded
             self.write_request(now)
             return self.func(*args, **kwargs)
         raise HTTPException(status_code=400, detail=limit)
 
-    def is_limited(self, time_now):
-        for seconds, limit in AMOUNT_LIMITS_CONFIG.items():  # {10: 1000, 60: 3000, 3600: 20000}
-            if limit <= self.get_requests_count(time_now, seconds):
-                return f'amount limit exeeded ({limit}/{seconds}sec)'
+    def is_limited(self, time_now: int) -> str or None:
+        for seconds, limit in AMOUNT_LIMITS_CONFIG.items():  # get conditions from AMOUNT_LIMITS_CONFIG and check the limit
+            if limit <= self.get_requests_count(time_now, seconds):  # if limit == requests per x seconds
+                return f'amount limit exceeded ({limit}/{seconds}sec)'
         return None
 
-    def write_request(self, time_now):
-        requests_per_second = Limiter.requests.get(time_now)
+    def write_request(self, time_now: int):
+        requests_per_second: int or None = Limiter.requests.get(
+            time_now)  # get the number of requests for the current second. "None" if the first request in this second
         if requests_per_second is None:
-            Limiter.requests[time_now] = 1
+            Limiter.requests[time_now] = 1  # create value
         else:
-            Limiter.requests[time_now] = requests_per_second + 1
+            Limiter.requests[time_now] = requests_per_second + 1  # rewrite value
 
-    def get_requests_count(self, time_now, seconds):
-        return sum([Limiter.requests.get(time_now - x, 0) for x in range(seconds)])
+    def get_requests_count(self, time_now: int, seconds: int) -> int:
+        return sum([Limiter.requests.get(time_now - x, 0) for x in
+                    range(seconds)])  # return the sum of all requests for a certain period of time
